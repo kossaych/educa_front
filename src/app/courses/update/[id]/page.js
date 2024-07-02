d4545c40457ce08856de9fa5ce01d308f62802fb
kossaych <cheminguikossay@gmail.com>
2024-07-02 18:49:10 +0100

    deployment

--- src/app/courses/update/[id]/page.js
+++ src/app/courses/update/[id]/page.js
@@ -671,11 +671,11 @@ function UpdateCourse(props) {
 
                                                         {course.levels.map(level => (  
                                                             <li key={level.id}>
-                                                            <div class="flex items-center p-2 rounded    ">
-                                                              <input id="checkbox-item-11" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" key={level.id} id={level.id} onClick={(e) => {handleLevelSelection(e)}} />  
-                                                              <label for="checkbox-item-11" class="w-full ms-2 text-sm font-medium text-gray-900 rounded  ">{level.title}</label>
-                                                            </div>
-                                                          </li> 
+                                                              <div key={level.id} class="flex items-center p-2 rounded ">
+                                                                <input id="checkbox-item-11" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" key={level.id} id={level.id} onClick={(e) => {handleLevelSelection(e)}} />  
+                                                                <label   class="w-full ms-2 text-sm font-medium text-gray-900 rounded  ">{level.title}</label>
+                                                              </div>
+                                                            </li> 
                                                             ))}
                                            
                                                      </ul>  
