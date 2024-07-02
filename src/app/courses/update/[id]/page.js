b214de0caab7dcdefd3c2e5349b9a59d0f3a356d
kossay chemingui <110374361+kossaych@users.noreply.github.com>
2024-07-02 20:18:39 +0100

    Update page.js

--- src/app/courses/update/[id]/page.js
+++ src/app/courses/update/[id]/page.js
@@ -672,7 +672,7 @@ function UpdateCourse(props) {
                                                         {course.levels.map(level => (  
                                                             <li key={level.id}>
                                                               <div key={level.id} class="flex items-center p-2 rounded ">
-                                                                <input id="checkbox-item-11" type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" key={level.id} id={level.id} onClick={(e) => {handleLevelSelection(e)}} />  
+                                                                <input   type="checkbox" value="" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"   id={level.id} onClick={(e) => {handleLevelSelection(e)}} />  
                                                                 <label   class="w-full ms-2 text-sm font-medium text-gray-900 rounded  ">{level.title}</label>
                                                               </div>
                                                             </li> 
