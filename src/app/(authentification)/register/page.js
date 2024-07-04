4007e35e6723def6a806340c83278317ed662a65
kossay chemingui <110374361+kossaych@users.noreply.github.com>
2024-07-04 16:05:53 +0100

    Update page.js
    deployment

--- src/app/(authentification)/register/page.js
+++ src/app/(authentification)/register/page.js
@@ -7,7 +7,7 @@ const RegistrationPage = () => {
     <div className = "bg-white rounded m-2 min-h-full grid grid-cols-1" >
          <Link href="/register_student" className="flex  mx-5 my-5 items-center justify-around align-middle bg-white text-blue-400 border border-blue-400 px-8 py-4 rounded-lg text-xl  ">
             <div  >
-              <CiApple size={50}></CiApple>
+               
             </div>
             <div  >
                 Student  
@@ -15,7 +15,7 @@ const RegistrationPage = () => {
          </Link>
           <Link href ="/register_teacher" className="flex items-center mx-5 my-5 justify-around align-middle bg-white text-blue-400 border border-blue-400 px-8 py-4 rounded-lg text-xl  ">
           <div  >
-              <CiApple size={50}></CiApple>
+               
             </div>
             <div  >
                 Teacher  
@@ -23,7 +23,7 @@ const RegistrationPage = () => {
           </Link>
           <Link href="/register_parent" className="flex items-center mx-5 my-5 justify-around align-middle bg-white text-blue-400 border border-blue-400 px-8 py-4 rounded-lg text-xl  ">
                 <div  >
-                  <CiApple size={50}></CiApple>
+                   
                 </div>
                 <div  >
                      Parent
